95ee56d54661425d2eabe005f00d6452c3be6a1d
tarun <tarun@gmail.com>
2019-03-06 06:28:20 +0000

    update camp description

--- seeds.js
+++ seeds.js
@@ -7,17 +7,17 @@ var data =[
 	{
 		name:"pushkar rajasthan",
 		image:"https://www.visittnt.com/blog/wp-content/uploads/2018/01/Camping-in-Rajasthan-1.jpg",
-		description:"this is in ajmer"
+		description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
 	},
 	{
 		name:"pushkar rajasthan",
 		image:"https://www.visittnt.com/blog/wp-content/uploads/2018/01/Camping-in-Rajasthan-1.jpg",
-		description:"this is in ajmer"
+		description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
 	},
 	{
 		name:"pushkar rajasthan",
 		image:"https://www.visittnt.com/blog/wp-content/uploads/2018/01/Camping-in-Rajasthan-1.jpg",
-		description:"this is in ajmer"
+		description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
 	}
 ];
 
